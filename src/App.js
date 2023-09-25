dde2ab9d5b2aabb5852c232e413fd702b2e1a5b7
shrenik49 <41732400+shrenik49@users.noreply.github.com>
2023-09-25 18:24:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Counter } from "./features/counter/Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
